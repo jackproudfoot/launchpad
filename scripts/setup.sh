511e572eda485c8dedd982fc7fa137b31c3fcd95
Jack Proudfoot <jsp50@duke.edu>
2022-02-21 20:05:14 -0500

    Added initial files

---
+++ scripts/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Z
+brew install z
+
+# Spaceship
+brew install spaceship
+
+# Colorls
+brew install ruby
+gem install colorls
